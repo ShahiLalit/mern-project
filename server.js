3a640071475895843e4a4c4937c3ca5646d09630
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-09-29 13:32:43 +0530

    MongoDB connection
    Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 8888;
 
+const mongoose = require('mongoose');
+
+// DB Config
+const db = require('./config/keys.json').mongoURI;
+
 app.get('/', (req, res) => res.send('Hello World!!'));
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log(`MongoDB Connected!!`))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
