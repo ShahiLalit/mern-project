ff5bef607a303b33a83cc6724e5f0d50e3fe3eae
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-09-30 19:08:48 +0530

    Added Validation to register and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User Model
 const User = require('./../../models/User');
@@ -69,13 +70,21 @@ router.post('/register', (req, res) => {
 // @access  Public
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
   User.findOne({ email })
     .then(user => {
       // Check for user
       if (!user) {
-        return res.status(404).json({ email: 'User Email Not Found' });
+        errors.email = 'User Not Found';
+        return res.status(404).json(errors);
       }
 
       // Check password
@@ -98,7 +107,8 @@ router.post('/login', (req, res) => {
               res.json({ success: true, token: `Bearer ${token}` })
           );
         } else {
-          return res.status(400).json({ password: 'Password Incorrect!' });
+          errors.password = 'Password incorrect';
+          return res.status(400).json(errors);
         }
       });
     })
