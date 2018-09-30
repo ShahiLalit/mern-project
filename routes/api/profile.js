357c25e74f16db287a34bb8e6d787f0f2719974c
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-09-30 21:41:30 +0530

    Create or Update user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,7 +7,8 @@ const passport = require('passport');
 const Profile = require('./../../models/Profile');
 
 // Load User Model
-const User = require('./../../models/User');
+// const User = require('./../../models/User');
+const User = mongoose.model('user');
 
 // @route   GET /api/profile/test
 // @desc    Testing Profile Route
@@ -32,6 +33,90 @@ router.get(
         res.status(200).json(profile);
       })
       .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   POST /api/profile
+// @desc    Create OR Edit User Profile
+// @access  Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    let errors = {};
+    // Get Fields for User Profile
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) {
+      profileFields.handle = req.body.handle;
+    }
+    if (req.body.company) {
+      profileFields.company = req.body.company;
+    }
+    if (req.body.website) {
+      profileFields.website = req.body.website;
+    }
+    if (req.body.location) {
+      profileFields.location = req.body.location;
+    }
+    if (req.body.bio) {
+      profileFields.bio = req.body.bio;
+    }
+    if (req.body.status) {
+      profileFields.status = req.body.status;
+    }
+    if (req.body.githubusername) {
+      profileFields.githubusername = req.body.githubusername;
+    }
+    // Split Skills into an Array
+    if (typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.skills.split(',');
+    }
+
+    // Social
+    profileFields.social = {};
+
+    if (req.body.youtube) {
+      profileFields.social.youtube = req.body.youtube;
+    }
+    if (req.body.facebook) {
+      profileFields.social.facebook = req.body.facebook;
+    }
+    if (req.body.twitter) {
+      profileFields.social.twitter = req.body.twitter;
+    }
+    if (req.body.linkedin) {
+      profileFields.social.linkedin = req.body.linkedin;
+    }
+    if (req.body.instagram) {
+      profileFields.social.instagram = req.body.instagram;
+    }
+
+    // Check if Profile Exist = Update OR Create Profile
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update the Profile
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Create the Profile
+        // But first check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = 'Handle already exists';
+            res.status(400).json(errors);
+          }
+
+          // Save Profile
+          new Profile(profileFields)
+            .save()
+            .then(profile => res.send(200).json(profile));
+        });
+      }
+    });
   }
 );
 
