e730e62c0f6e21ff149a632320046838750be207
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-10-07 10:53:26 +0530

    route info

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -330,6 +330,9 @@ router.delete(
 //   }
 // );
 
+// @route   DELETE /api/profile
+// @desc    Delete User Profile and User
+// @access  Private
 router.delete(
   '/',
   passport.authenticate('jwt', { session: false }),
