58bf1efd60aae6add00d9ce7b9afc0aab84512ec
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-10-13 20:08:44 +0530

    Added layout components

---
+++ client/src/components/layouts/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => {
+  return (
+    <footer className="">
+      Copyright &copy; {new Date().getFullYear()} DevConnector
+    </footer>
+  );
+};
+
+export default footer;
