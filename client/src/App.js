d3b9b1263b0a67eecdd4203c2700e7edf3969a29
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-10-13 20:47:29 +0530

    Added login and register
    Linked the components to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
-import Navbar from './components/layouts/Navbar';
-import Footer from './components/layouts/Footer';
-import Landing from './components/layouts/Landing';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
