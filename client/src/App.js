4e2e7d59ebb9b569746ee1b9bef63c57fa6789c8
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-10-14 21:24:29 +0530

    Linked CreateProfile

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 // Check for jwtToken in the localStorage on refresh or routing to a page
 
@@ -58,6 +59,13 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
