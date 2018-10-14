544e81dfa8186aebec18b754256f4e62d019119e
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-10-14 12:56:47 +0530

    Pertianing the user details on refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import setAuthToken from './utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+import { setCurrentUser } from './actions/authActions';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -11,6 +15,15 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+// Check for jwtToken in the localStorage on refresh or routing to a page
+if (localStorage.jwtToken) {
+  // Set Auth Token to Header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode the token to get User data and Token expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set Current User and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 class App extends Component {
   render() {
     return (
