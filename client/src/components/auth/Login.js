0a48d28896e7940365e1559d53423d9b56dcc47a
Lalit Singh Shahi <shahilalit002@gmail.com>
2018-10-14 14:13:42 +0530

    Redirecting logged in user from register and login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -25,6 +25,12 @@ export class Login extends Component {
     this.props.loginUser(userData);
   };
 
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
       this.props.history.push('/dashboard');
